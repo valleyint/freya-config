a759179add0835c2286964955254dc2538d2748f
valleyint <darshan@melkote.com>
2026-05-27 11:09:14 +0530

    cleanup

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "catppuccin",
-          globalstatus = true,
-        },
-      })
-    end,
-  },
-  {
-    "echasnovski/mini.animate",
-    version = "*",
-    config = function()
-      require("mini.animate").setup()
-    end,
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin-mocha",
+					globalstatus = true,
+				},
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.animate",
+		version = "*",
+		config = function()
+			require("mini.animate").setup()
+		end,
+	},
 }
